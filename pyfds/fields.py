e33dd4b4dc81375e57770789d01e8aba832329eb
Leander Claes <claes@emt.uni-paderborn.de>
2017-01-02 16:09:00 +0100

    Add FieldComponent class.

--- pyfds/fields.py
+++ pyfds/fields.py
@@ -22,3 +22,35 @@ class Dimension:
         assert len(index) > 0, "No point found within snap radius of given value."
 
         return int(index)
+
+
+class FieldComponent:
+    """A single component of a field (e.g. electric field in the x direction)."""
+
+    def __init__(self, num_points):
+
+        # values of the field component
+        self.values = np.zeros(num_points)
+        # list with objects of type Boundary
+        self.boundaries = []
+        # list with objects of type Output
+        self.outputs = []
+
+    def apply_bounds(self):
+        """Applies the  boundary conditions to the field component."""
+
+        for bound in self.boundaries:
+            self.values[bound.region.indices] = (bound.additive *
+                                                 self.values[bound.region.indices] + bound.value)
+
+    def write_outputs(self):
+        """Writes the values of the field component to the outputs."""
+
+        for output in self.outputs:
+
+            if not output.signals:
+                output.signals = [[self.values[index]] for index in output.region.indices]
+            else:
+                [signal.append(self.values[index]) for index, signal in
+                 zip(output.region.indices, output.signals)]
+
