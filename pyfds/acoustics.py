6cfb065b480567408d5eb7a771968f03ca7d15c3
Leander Claes <claes@emt.uni-paderborn.de>
2017-01-04 11:01:39 +0100

    Add class Acoustic2D. First working 2D simulation environment.

--- pyfds/acoustics.py
+++ pyfds/acoustics.py
@@ -40,6 +40,63 @@ class Acoustic1D(fld.Field1D):
             self.pressure.values -= self.a_pv.dot(self.velocity.values)
 
 
+class Acoustic2D(fld.Field2D):
+    """Class for simulation of two dimensional acoustic fields."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.pressure = fld.FieldComponent(self.num_points)
+        self.velocity_x = fld.FieldComponent(self.num_points)
+        self.velocity_y = fld.FieldComponent(self.num_points)
+
+        # initialize attributes sparse matrices
+        self.a_p_vx = None
+        self.a_p_vy = None
+        self.a_vx_p = None
+        self.a_vy_p = None
+        self.a_vx_vx = None
+        self.a_vy_vy = None
+
+    def simulate(self):
+        """Starts the simulation."""
+
+        self.a_p_vx = self.d_x(factors=(self.t.increment / self.x.increment *
+                                        self.material_vector('sound_velocity') ** 2 *
+                                        self.material_vector('density')))
+        self.a_p_vy = self.d_y(factors=(self.t.increment / self.y.increment *
+                                        self.material_vector('sound_velocity') ** 2 *
+                                        self.material_vector('density')))
+        self.a_vx_p = self.d_x(factors=(self.t.increment / self.x.increment /
+                                        self.material_vector('density')), backward=True)
+        self.a_vy_p = self.d_y(factors=(self.t.increment / self.y.increment /
+                                        self.material_vector('density')), backward=True)
+        self.a_vx_vx = (self.d_x2(factors=(self.t.increment / self.x.increment ** 2 *
+                                           self.material_vector('absorption_coef') /
+                                           self.material_vector('density'))) +
+                        self.d_y2(factors=(self.t.increment / self.y.increment ** 2 *
+                                           self.material_vector('absorption_coef') /
+                                           self.material_vector('density'))))
+        self.a_vy_vy = self.a_vx_vx
+
+        for ii in range(self.t.samples):
+
+            self.pressure.apply_bounds(ii)
+            self.pressure.write_outputs()
+
+            self.velocity_x.values -= (self.a_vx_p.dot(self.pressure.values) -
+                                       self.a_vx_vx.dot(self.velocity_x.values))
+            self.velocity_y.values -= (self.a_vy_p.dot(self.pressure.values) -
+                                       self.a_vy_vy.dot(self.velocity_y.values))
+
+            self.velocity_x.apply_bounds(ii)
+            self.velocity_x.write_outputs()
+            self.velocity_y.apply_bounds(ii)
+            self.velocity_y.write_outputs()
+
+            self.pressure.values -= (self.a_p_vx.dot(self.velocity_x.values) +
+                                     self.a_p_vy.dot(self.velocity_y.values))
+
+
 class AcousticMaterial:
     """Class for specification of acoustic material parameters."""
 
