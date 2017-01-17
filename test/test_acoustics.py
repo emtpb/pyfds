5d8b217659fdd4a7248a60b430a24fe909bca805
Leander Claes <claes@emt.uni-paderborn.de>
2017-01-17 11:08:01 +0100

    Add test case for Acoustic1D.create_matrices().

--- test/test_acoustics.py
+++ test/test_acoustics.py
@@ -7,3 +7,13 @@ def test_acoustic_material():
     water.bulk_viscosity = 1e-3
     water.shear_viscosity = 1e-3
     assert np.isclose(water.absorption_coef, 7e-3 / 3)
+
+
+def test_acoustic1d_create_matrices():
+    fld = fds.Acoustic1D(t_delta=1, t_samples=1,
+                         x_delta=1, x_samples=3,
+                         material=fds.AcousticMaterial(700, 0.01, bulk_viscosity=1))
+    fld.create_matrices()
+    assert np.allclose(fld.a_p_v.toarray(), [[-4900, 4900, 0], [0, -4900, 4900], [0, 0, -4900]])
+    assert np.allclose(fld.a_v_p.toarray(), [[100, 0, 0], [-100, 100, 0], [0, -100, 100]])
+    assert np.allclose(fld.a_v_v.toarray(), [[-200, 100, 0], [100, -200, 100], [0, 100, -200]])
