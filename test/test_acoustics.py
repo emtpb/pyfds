eef828b3ca5e0709829db24b3e43a64280076761
Leander Claes <claes@emt.uni-paderborn.de>
2017-01-17 11:27:18 +0100

    Add test case for Acoustic2D.create_matrices().

--- test/test_acoustics.py
+++ test/test_acoustics.py
@@ -17,3 +17,23 @@ def test_acoustic1d_create_matrices():
     assert np.allclose(fld.a_p_v.toarray(), [[-4900, 4900, 0], [0, -4900, 4900], [0, 0, -4900]])
     assert np.allclose(fld.a_v_p.toarray(), [[100, 0, 0], [-100, 100, 0], [0, -100, 100]])
     assert np.allclose(fld.a_v_v.toarray(), [[-200, 100, 0], [100, -200, 100], [0, 100, -200]])
+
+
+def test_acoustic2d_create_matrices():
+    fld = fds.Acoustic2D(t_delta=1, t_samples=1,
+                         x_delta=1, x_samples=2,
+                         y_delta=1, y_samples=2,
+                         material=fds.AcousticMaterial(700, 0.01, bulk_viscosity=1))
+    fld.create_matrices()
+    assert np.allclose(fld.a_p_vx.toarray(), [[-4900, 4900, 0, 0], [0, -4900, 4900, 0],
+                                              [0, 0, -4900, 4900], [0, 0, 0, -4900]])
+    assert np.allclose(fld.a_p_vy.toarray(), [[-4900, 0, 4900, 0], [0, -4900, 0, 4900],
+                                              [0, 0, -4900, 0], [0, 0, 0, -4900]])
+    assert np.allclose(fld.a_vx_p.toarray(), [[100, 0, 0, 0], [-100, 100, 0, 0], [0, -100, 100, 0],
+                                              [0, 0, -100, 100]])
+    assert np.allclose(fld.a_vy_p.toarray(), [[100, 0, 0, 0], [0, 100, 0, 0], [-100, 0, 100, 0],
+                                              [0, -100, 0, 100]])
+    assert np.allclose(fld.a_vx_vx.toarray(), [[-400, 100, 100, 0], [100, -400, 100, 100],
+                                               [100, 100, -400, 100], [0, 100, 100, -400]])
+    assert np.allclose(fld.a_vy_vy.toarray(), [[-400, 100, 100, 0], [100, -400, 100, 100],
+                                               [100, 100, -400, 100], [0, 100, 100, -400]])
