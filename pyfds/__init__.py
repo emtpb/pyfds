bf50623b5d763c566d31ae8fdf8c25dfc73f3ec3
Leander Claes <claes@emt.uni-paderborn.de>
2017-04-27 13:46:29 +0200

    Update __init__.py

--- pyfds/__init__.py
+++ pyfds/__init__.py
@@ -1,3 +1,4 @@
 from .regions import Boundary, MaterialRegion, Output
 from .acoustics import Acoustic1D, Acoustic2D, Acoustic3DAxi, AcousticMaterial
+from .nonlinear_acoustics import NonlinearAcoustic1D
 from .gfx import Animator1D, Animator2D
