efafe0f5da745a70fc0bd3d5dffe66ac1c315e5a
Leander Claes <claes@emt.uni-paderborn.de>
2021-10-21 12:25:05 +0200

    Add one-dimensional thermal simulation.

---
+++ pyfds/thermal.py
@@ -0,0 +1,60 @@
+from . import fields as fld
+
+
+class Thermal1D(fld.Field1D):
+    """Class for simulation of one-dimensional thermal fields."""
+
+    def __init__(self, *args, **kwargs):
+        """Class constructor.
+
+        Args:
+            See pyfds.fields.Field1D constructor arguments.
+        """
+        super().__init__(*args, **kwargs)
+        self.temperature = fld.FieldComponent(self.num_points)
+        self.heat_flux = fld.FieldComponent(self.num_points)
+
+        # initialize attributes sparse matrices
+        self.a_t_q = None
+        self.a_q_t = None
+
+    def assemble_matrices(self):
+        """Assemble the a_* matrices required for simulation."""
+
+        self.a_t_q = self.d_x(factors=(self.t.increment / self.x.increment /
+                                       self.material_vector('density') /
+                                       self.material_vector('heat_capacity')))
+        self.a_q_t = self.d_x(factors=(1 / self.x.increment *
+                                       self.material_vector('thermal_conductivity')),
+                              variant='backward')
+        self.matrices_assembled = True
+
+    def sim_step(self):
+        """Simulate one step."""
+
+        self.temperature.apply_bounds(self.step)
+        self.temperature.write_outputs()
+
+        self.heat_flux.values = -self.a_q_t.dot(self.temperature.values)
+
+        self.heat_flux.apply_bounds(self.step)
+        self.heat_flux.write_outputs()
+
+        self.temperature.values -= self.a_t_q.dot(self.heat_flux.values)
+
+
+class ThermalMaterial:
+    """Class for specification of thermal material parameters."""
+
+    def __init__(self, heat_capacity, density, thermal_conductivity):
+        """Class constructor. Default values for optional parameters create lossless medium.
+
+        Args:
+            heat_capacity: Specific heat capacity.
+            density: Density.
+            thermal_conductivity: Thermal conductivity coefficient.
+        """
+
+        self.heat_capacity = heat_capacity
+        self.density = density
+        self.thermal_conductivity = thermal_conductivity
