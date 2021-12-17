8a5d8e08fa8dc9a056aa2b3566108247c5d8498a
Leander Claes <claes@emt.uni-paderborn.de>
2021-12-17 15:16:50 +0100

    Add one-dimensional electrostatic simulation.

--- pyfds/__init__.py
+++ pyfds/__init__.py
@@ -2,4 +2,5 @@ from .regions import Boundary, MaterialRegion, Output
 from .acoustics import Acoustic1D, Acoustic2D, Acoustic3DAxi, AcousticMaterial
 from .nonlinear_acoustics import IdealGas1D, Acoustic2ndOrder1D, AcousticMaterial2ndOrder
 from .thermal import Thermal1D, Thermal2D, Thermal3DAxi, ThermalMaterial
+from .electrostatics import Electrostatic1D, ElectrostaticMaterial
 from .gfx import Animator1D, Animator2D
---
+++ pyfds/electrostatics.py
@@ -0,0 +1,56 @@
+import logging as lo
+import scipy.sparse as sp
+import scipy.sparse.linalg as sl
+from . import fields as fld
+
+logger = lo.getLogger('pyfds')
+
+
+class Electrostatic1D(fld.Field1D):
+    """Class for simulation of one-dimensional electrostatic fields."""
+
+    def __init__(self, x_samples, x_delta, material):
+        """Class constructor.
+
+        Args:
+            x_samples: Number of samples in x direction.
+            x_delta: Increment in x direction.
+            material: Main material of the field.
+        """
+
+        super().__init__(x_samples, x_delta, 1, 1, material)
+        self.potential = fld.FieldComponent(self.num_points)
+        self.charge_density = fld.FieldComponent(self.num_points)
+
+        # initialize attributes sparse matrices
+        self.a_phi_rho = None
+
+    def assemble_matrices(self):
+        """Assemble the a_* matrices required for simulation."""
+
+        # Conversion to Compressed Sparse Column format is necessary for efficient inversion
+        a_rho_phi = sp.csc_matrix(
+            self.d_x2(factors=(self.material_vector('permittivity') /
+                               self.x.increment ** 2)))
+        self.a_phi_rho = sl.inv(a_rho_phi)
+        self.matrices_assembled = True
+
+    def sim_step(self):
+        """Simulate one step."""
+
+        self.charge_density.apply_bounds(self.step)
+        self.charge_density.write_outputs()
+
+        self.potential.values = -self.a_phi_rho.dot(self.charge_density.values)
+
+
+class ElectrostaticMaterial:
+    """Class for specification of electrostatic material parameters."""
+
+    def __init__(self, permittivity=8.8541878128e-12):
+        """Class constructor. Default values create vacuum.
+
+        Args:
+            permittivity: Permittivity in As/Vm.
+        """
+        self.permittivity = permittivity
