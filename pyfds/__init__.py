ac6dab1569a3db0f1ac40a597dcf857d6ad37738
Leander Claes <claes@emt.uni-paderborn.de>
2021-12-17 16:19:21 +0100

    Add two-dimensional electrostatic simulation.

--- pyfds/__init__.py
+++ pyfds/__init__.py
@@ -2,5 +2,5 @@ from .regions import Boundary, MaterialRegion, Output
 from .acoustics import Acoustic1D, Acoustic2D, Acoustic3DAxi, AcousticMaterial
 from .nonlinear_acoustics import IdealGas1D, Acoustic2ndOrder1D, AcousticMaterial2ndOrder
 from .thermal import Thermal1D, Thermal2D, Thermal3DAxi, ThermalMaterial
-from .electrostatics import Electrostatic1D, ElectrostaticMaterial
+from .electrostatics import Electrostatic1D, Electrostatic2D, ElectrostaticMaterial
 from .gfx import Animator1D, Animator2D
--- pyfds/electrostatics.py
+++ pyfds/electrostatics.py
@@ -51,6 +51,54 @@ class Electrostatic1D(fld.Field1D):
                 'Boundary conditions for electric potential are currently not supported.')
 
 
+class Electrostatic2D(fld.Field2D):
+    """Class for simulation of two-dimensional electrostatic fields."""
+
+    def __init__(self, x_samples, x_delta, y_samples, y_delta, material):
+        """Class constructor.
+
+        Args:
+            x_samples: Number of samples in x direction.
+            x_delta: Increment in x direction.
+            y_samples: Number of samples in y direction.
+            y_delta: Increment in y direction.
+            material: Main material of the field.
+        """
+
+        super().__init__(x_samples, x_delta, y_samples, y_delta, 1, 1, material)
+        self.potential = fld.FieldComponent(self.num_points)
+        self.charge_density = fld.FieldComponent(self.num_points)
+
+        # initialize attributes sparse matrices
+        self.a_phi_rho = None
+
+    def assemble_matrices(self):
+        """Assemble the a_* matrices required for simulation."""
+
+        # Conversion to Compressed Sparse Column format is necessary for efficient inversion
+        a_rho_phi = sp.csc_matrix(
+            self.d_x2(factors=(self.material_vector('permittivity') /
+                               self.x.increment ** 2)) +
+            self.d_y2(factors=(self.material_vector('permittivity') /
+                               self.y.increment ** 2)))
+        self.a_phi_rho = sl.inv(a_rho_phi)
+        self.matrices_assembled = True
+
+    def sim_step(self):
+        """Simulate one step."""
+
+        self.charge_density.apply_bounds(self.step)
+        self.charge_density.write_outputs()
+
+        self.potential.values = -self.a_phi_rho.dot(self.charge_density.values)
+
+        if len(self.potential.boundaries) != 0:
+            wn.warn('Boundary conditions for electric potential are currently not supported.',
+                    stacklevel=2)
+            logger.warning(
+                'Boundary conditions for electric potential are currently not supported.')
+
+
 class ElectrostaticMaterial:
     """Class for specification of electrostatic material parameters."""
 
